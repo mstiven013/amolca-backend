32b5c949eded4cfa3fe21e9f0944702c3f70bc86
Stiven Lopez <32859798+mstiven013@users.noreply.github.com>
2018-08-02 15:33:53 -0500

    Books Model

---
+++ src/components/books/BooksModel.js
@@ -0,0 +1,99 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BookModel = new Schema({
+    attributes: [{
+        id: String,
+        value: String
+    }],
+    author: [{
+        type: String,
+        required: true
+    }],
+    description: String,
+    index: String,
+    inventory: {
+        quantity: { 
+            type: Number,
+            default: 0
+        },
+        isbn: {
+            type: String
+        },
+        state: {
+            type: String,
+            default: "STOCK",
+            enum: ["RESERVED", "SPENT", "STOCK"]
+        },
+        individualSale: {
+            type: Boolean,
+            default: false
+        },
+        allowReservations: {
+            type: Boolean,
+            default: false
+        },
+        reservationNote: {
+            type: String
+        }
+    },
+    image: [{
+        name: {
+            type: String,
+            default: 'image-not-found.jpg'
+        },
+        principal: {
+            type: Boolean
+        }
+    }],
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    publicationYear: {
+        type: Number,
+        required: true
+    },
+    relatedProducts: [{
+        type: String
+    }],
+    userId: String,
+    variations: [{
+        description: String,
+        name: String,
+        price: Number,
+        image: String,
+        inventory: {
+            quantity: Number,
+            isbn: String,
+            state: {
+                type: String,
+                enum: ["RESERVED", "SPENT", "STOCK"]
+            },
+            individualSale: {
+                type: Boolean,
+                default: false
+            }
+        }
+    }],
+    version: [{
+        type: String,
+        enum: ["PAPER", "EBOOK", "VIDEO"],
+        default: "PAPER"
+    }],
+    visibility: {
+        type: String,
+        default: "ALL",
+        enum: ["ALL", "HOME", "SHOP", "SPECIALTY"]
+    },
+    volume: {
+        type: Number,
+        default: 1
+    }
+});
