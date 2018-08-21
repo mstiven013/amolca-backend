160a07fb9d6481bcd2af7d9bdb2c80af11d79dd2
Stiven Lopez <32859798+mstiven013@users.noreply.github.com>
2018-08-21 10:57:37 -0500

    Fix dbPass variable

--- src/config.js
+++ src/config.js
@@ -8,9 +8,9 @@ const config = {
     secret: 'mysecretkeyapi',
     dbPass: encodeURIComponent('AmolcaColombia%2018'),    
     dbAuth: {},
-    //dbAuth: { auth: { user: 'amolca', password: 'AmolcaColombia%2018' }},
+    dbAuth: { auth: { user: 'amolca', password: 'AmolcaColombia%2018' }},
     //db: 'mongodb://localhost:27017/amolca-store',
-    db: `mongodb+srv://amolca:${dbPass}@amolcaweb2018-pxfid.mongodb.net/test`
+    db: `mongodb+srv://amolca:${this.dbPass}@amolcaweb2018-pxfid.mongodb.net/test`
 }
 
 module.exports = config;
