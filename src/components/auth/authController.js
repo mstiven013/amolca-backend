9c9e7e4d78541812608ec7ad012d737929fece8a
mstiven013 <32859798+mstiven013@users.noreply.github.com>
2018-11-29 20:20:01 -0500

    Change email sender in authentication

--- src/components/auth/authController.js
+++ src/components/auth/authController.js
@@ -1,11 +1,18 @@
 'use strict'
 
+const config = require('../../config');
 const mongoose = require('mongoose');
 const User = require('../users/UsersModel');
 const TokenService = require('./tokenService');
 const nodemailer = require('nodemailer');
 const xoauth2 = require('xoauth2');
 const bcrypt = require('bcrypt-nodejs');
+const handlebars = require('handlebars');
+const hbsHelpers = require('handlebars-helpers');
+const allHelpers = hbsHelpers.math();
+const moment = require('moment');
+const sendmail = require('sendmail')();
+const fs = require('fs');
 
 //Controller register an User
 async function signUp(req, res) {
@@ -33,6 +40,48 @@ async function signUp(req, res) {
             });
         }
 
+        sendmail({
+            from: `Amolca Colombia <ventas@amolca.com.co>`,
+            to: userStored.email,
+            subject: `¡Hola, ${userStored.name}! Te damos la bienvenida a Amolca`,
+            html: `
+                    <div style="width: 100%; background-color: #f2f2f2; padding-bottom: 20px; padding-top: 20px;">
+                        <div style="background-color: #fff; border-radius: 10px; overflow: hidden; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; -ms-box-sizing: border-box; box-sizing: border-box;  margin-left: auto; margin-right: auto; width: 600px;">
+                            <!--Header email-->
+                            <div style="background-color: #f9f9f9; border-bottom: 1px solid #ccc; border-top-left-radius: 10px; border-top-right-radius: 10px; padding: 30px 50px; text-align: center;">
+                                <a href="http://www.amolca.com/" target="_blank"><img src="http://amolca.webussines.com/assets/img/common/logo.png" width="auto" height="80px" alt=""></a>
+                            </div>
+                            <!--Body email-->
+                            <div style="color: #AAAAAA; font-size: 16px; line-height: normal; padding: 50px; font-family: Arial;">
+                                <p style="font-size: 20px; font-weight: bold; margin-top: 0px;">¡Hola, <span style="color: #00396F;">${userStored.name}</span>!</p>
+                                <p>Te queremos dar la bienvenida a nuestra familia y decirte que estaremos siempre pendientes de brindarte la atención que mereces.</p>
+                                <p>Si tienes dudas, problemas, inquietudes o sugerencias no dudes en contactarnos y con mucho gusto responderemos a todas tus solicitudes.</p>
+                            </div>
+                            <!--Footer email-->
+                            <div style="background-color: #00396F; color: #FFF; font-family: Arial; padding: 30px; text-align: center;">
+                                <p style="margin: 0px;">
+                                    <a href="http://www.amolca.com" target="_blank" style="color: #FFF;">Amolca Editorial médica y odontológica</a>
+                                </p>
+                                <p style="color: #FFF; margin-bottom: 5px;">©2018</p>
+                                <p style="margin: 0px;">
+                                    <a style="color: #FFF;">Condiciones de servicio & Política de privacidad</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    `
+        }, (err, reply) => {
+            if(err) {
+                console.log('Error enviando el email')
+                return rej({ status: 500, message: `An error has ocurred sending the message to your email` });
+            } else {
+                console.log('Enviado correctamente')
+                //Send status and user stored
+                return res('Enviado correctamente');
+            }
+        })
+
+        /*
         const smtpTransport = nodemailer.createTransport({
             service: 'gmail',
             host: 'smtp.gmail.com',
@@ -43,40 +92,15 @@ async function signUp(req, res) {
                 pass: 'SoloNacional960723'
             }
         });
-    
+        
         const mailOptions = {
             from: `Amolca Colombia <info@amolca.com.co>`,
             to: userStored.email,
             subject: `¡Hola, ${userStored.name}! Te damos la bienvenida a Amolca`,
             text: `¡Hola, ${userStored.name}! Bienvenido a Amolca, esperamos nuestra relación sea feliz y duradera.`,
-            html: `
-            <div style="width: 100%; background-color: #f2f2f2; padding-bottom: 20px; padding-top: 20px;">
-                <div style="background-color: #fff; border-radius: 10px; overflow: hidden; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; -ms-box-sizing: border-box; box-sizing: border-box;  margin-left: auto; margin-right: auto; width: 600px;">
-                    <!--Header email-->
-                    <div style="background-color: #f9f9f9; border-bottom: 1px solid #ccc; border-top-left-radius: 10px; border-top-right-radius: 10px; padding: 30px 50px; text-align: center;">
-                        <a href="http://www.amolca.com/" target="_blank"><img src="http://amolca.webussines.com/assets/img/common/logo.png" width="auto" height="80px" alt=""></a>
-                    </div>
-                    <!--Body email-->
-                    <div style="color: #AAAAAA; font-size: 16px; line-height: normal; padding: 50px; font-family: Arial;">
-                        <p style="font-size: 20px; font-weight: bold; margin-top: 0px;">¡Hola, <span style="color: #00396F;">${userStored.name}</span>!</p>
-                        <p>Te queremos dar la bienvenida a nuestra familia y decirte que estaremos siempre pendientes de brindarte la atención que mereces.</p>
-                        <p>Si tienes dudas, problemas, inquietudes o sugerencias no dudes en contactarnos y con mucho gusto responderemos a todas tus solicitudes.</p>
-                    </div>
-                    <!--Footer email-->
-                    <div style="background-color: #00396F; color: #FFF; font-family: Arial; padding: 30px; text-align: center;">
-                        <p style="margin: 0px;">
-                            <a href="http://www.amolca.com" target="_blank" style="color: #FFF;">Amolca Editorial médica y odontológica</a>
-                        </p>
-                        <p style="color: #FFF; margin-bottom: 5px;">©2018</p>
-                        <p style="margin: 0px;">
-                            <a style="color: #FFF;">Condiciones de servicio & Política de privacidad</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            `
+            html: 
         }
-    
+        
         smtpTransport.sendMail(mailOptions, (err, response) => {
     
             if(err) {
@@ -90,6 +114,7 @@ async function signUp(req, res) {
             }
     
         });
+        */
 
     });
 }
