c3db194731e7c645cd2ae1b30393d86c7e36edb7
mstiven013 <32859798+mstiven013@users.noreply.github.com>
2018-12-13 12:01:54 -0500

    Modificar controlador de registro por una Promise

--- src/components/auth/authController.js
+++ src/components/auth/authController.js
@@ -13,17 +13,19 @@ const allHelpers = hbsHelpers.math();
 const moment = require('moment');
 const sendmail = require('sendmail')();
 const fs = require('fs');
+const emails = require('../emails/emailsAccount');
 
 //Controller register an User
 async function signUp(req, res) {
-    if (!req.body.name || !req.body.email || !req.body.role || !req.body.country) {
+    
+    if (!req.body.user || !req.body.user.name || !req.body.user.email || !req.body.user.role || !req.body.user.country) {
         return res.status(400).send({
             status: 400,
             message: 'Bad request'
         })
     }
 
-    let user = new User(req.body);
+    let user = new User(req.body.user);
 
     user.save(function (err, userStored) {
         if(err && err.code === 11000) {
@@ -40,81 +42,16 @@ async function signUp(req, res) {
             });
         }
 
-        sendmail({
-            from: `Amolca Colombia <ventas@amolca.com.co>`,
-            to: userStored.email,
-            subject: `¡Hola, ${userStored.name}! Te damos la bienvenida a Amolca`,
-            html: `
-                    <div style="width: 100%; background-color: #f2f2f2; padding-bottom: 20px; padding-top: 20px;">
-                        <div style="background-color: #fff; border-radius: 10px; overflow: hidden; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; -ms-box-sizing: border-box; box-sizing: border-box;  margin-left: auto; margin-right: auto; width: 600px;">
-                            <!--Header email-->
-                            <div style="background-color: #f9f9f9; border-bottom: 1px solid #ccc; border-top-left-radius: 10px; border-top-right-radius: 10px; padding: 30px 50px; text-align: center;">
-                                <a href="http://www.amolca.com/" target="_blank"><img src="http://amolca.webussines.com/assets/img/common/logo.png" width="auto" height="80px" alt=""></a>
-                            </div>
-                            <!--Body email-->
-                            <div style="color: #AAAAAA; font-size: 16px; line-height: normal; padding: 50px; font-family: Arial;">
-                                <p style="font-size: 20px; font-weight: bold; margin-top: 0px;">¡Hola, <span style="color: #00396F;">${userStored.name}</span>!</p>
-                                <p>Te queremos dar la bienvenida a nuestra familia y decirte que estaremos siempre pendientes de brindarte la atención que mereces.</p>
-                                <p>Si tienes dudas, problemas, inquietudes o sugerencias no dudes en contactarnos y con mucho gusto responderemos a todas tus solicitudes.</p>
-                            </div>
-                            <!--Footer email-->
-                            <div style="background-color: #00396F; color: #FFF; font-family: Arial; padding: 30px; text-align: center;">
-                                <p style="margin: 0px;">
-                                    <a href="http://www.amolca.com" target="_blank" style="color: #FFF;">Amolca Editorial médica y odontológica</a>
-                                </p>
-                                <p style="color: #FFF; margin-bottom: 5px;">©2018</p>
-                                <p style="margin: 0px;">
-                                    <a style="color: #FFF;">Condiciones de servicio & Política de privacidad</a>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    `
-        }, (err, reply) => {
-            if(err) {
-                console.log('Error enviando el email')
-                return res.status(500).send({ status: 500, message: `An error has ocurred sending the message to your email` });
-            } else {
+        emails.send(userStored, req.body.mailer)
+            .then((resp) => {
                 console.log('Enviado correctamente')
                 //Send status and user stored
                 return res.status(200).send({user: userStored, status: 200, message: 'Logged successfully', access_token: TokenService.createToken(userStored)});
-            }
-        })
-
-        /*
-        const smtpTransport = nodemailer.createTransport({
-            service: 'gmail',
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true,
-            auth: {
-                user: 'mstiven013@gmail.com',
-                pass: 'SoloNacional960723'
-            }
-        });
-        
-        const mailOptions = {
-            from: `Amolca Colombia <info@amolca.com.co>`,
-            to: userStored.email,
-            subject: `¡Hola, ${userStored.name}! Te damos la bienvenida a Amolca`,
-            text: `¡Hola, ${userStored.name}! Bienvenido a Amolca, esperamos nuestra relación sea feliz y duradera.`,
-            html: 
-        }
-        
-        smtpTransport.sendMail(mailOptions, (err, response) => {
-    
-            if(err) {
-                console.log('Error enviando el email') 
-                console.log(err)
-                return res.status(500).send({ status: 500, message: `An error has ocurred sending the message to your email` });
-            } else {
-                console.log('Enviado correctamente')
-                //Send status and user stored
-                return res.status(201).send({'access_token': TokenService.createToken(user), user: user});
-            }
-    
-        });
-        */
+            })
+            .catch((e) => {
+                console.log('Error enviando el email')
+                //return res.status(500).send({ status: 500, message: `An error has ocurred sending the message to your email` });
+            })
 
     });
 }
---
+++ src/components/emails/emailsAccount.js
@@ -0,0 +1,78 @@
+'use strict'
+
+const config = require('../../config');
+const nodemailer = require('nodemailer');
+const handlebars = require('handlebars');
+const hbsHelpers = require('handlebars-helpers');
+const allHelpers = hbsHelpers.math();
+const moment = require('moment');
+const sendmail = require('sendmail')();
+const fs = require('fs');
+const controller = {};
+
+handlebars.registerHelper('getBookTitle', function(arr, id) {
+    return arr.this.title
+});
+
+const readHTMLFile = function(path, callback) {
+    fs.readFile(path, {encoding: 'utf-8'}, function (err, html) {
+        if (err) {
+            throw err;
+            callback(err);
+        }
+        else {
+            callback(null, html);
+        }
+    });
+};
+
+
+controller.send = (user, mailer) => {
+    
+    const email = new Promise( (res, rej) => {
+
+        try {
+
+            readHTMLFile('src/public/email/register.html', function(err, html) {
+                var template = handlebars.compile(html);
+                var replacements = {
+                    name: user.name,
+                    domain: mailer.domain
+                };
+
+                var htmlToSend = template(replacements);
+
+                sendmail({
+                    from: mailer.from,
+                    to: mailer.to,
+                    cc: mailer.cc,
+                    subject: `¡Hola, ${user.name}! Te damos la bienvenida a Amolca`,
+                    html: htmlToSend
+                }, (err, reply) => {
+                    if(err) {
+                        console.log('Error enviando el email')
+                        return rej({ status: 500, message: `An error has ocurred sending the message to your email` });
+                    } else {
+                        console.log('Enviado correctamente')
+                        //Send status and user stored
+                        return res('Enviado correctamente');
+                    }
+                })
+            });
+
+           res(200)
+
+        } catch(e) {
+            //Reject if an error has ocurred
+            rej({
+                status: 500,
+                message: `An error has ocurred in server: ${e}`
+            })
+        }
+    })
+
+    return email;
+
+}
+
+module.exports = controller;
